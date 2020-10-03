874dcb971c7cb3112ca55f444743636f520ae69d
Gary Kerr <garykerrwork@gmail.com>
2020-10-03 16:07:17 +0100

    Remove Linux test configuration

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import VisionTestRowFinder1Tests
-
-var tests = [XCTestCaseEntry]()
-tests += VisionTestRowFinder1Tests.allTests()
-XCTMain(tests)
--- Tests/VisionTestRowFinder1Tests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(VisionTestRowFinder1Tests.allTests),
-    ]
-}
-#endif
